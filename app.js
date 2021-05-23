a00d019b2bacc676f76d0335ba062999fe637ad8
Nomaira <nomairafariah@outlook.com>
2021-05-23 17:25:30 +0100

    updated CSS and JS

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const countdown = () => {
-    const setCount = new Date('June 10, 2021 00:00:00').getTime();
+    const setDate = new Date('June 10, 2021 00:00:00').getTime();
     const now = new Date().getTime();
-    const countingDown = setCount - now;
+    const countingDown = setDate - now;
 
+    // Specifying Time
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
-    const day = hour % 24;
+    const day = hour * 24;
+ 
+    // Calculate the date
+    const textDay = Math.floor(countingDown / day);
+    const textHour = Math.floor((countingDown % day) / hour);
+    const textMinute = Math.floor((countingDown % hour) / minute);
+    const textSecond = Math.floor((countingDown % minute) / second);
 
-    document.querySelector(".day").innerHTML = day;
-    document.querySelector(".hour").innerHTML = day;
-    document.querySelector(".minute").innerHTML = day;
-    document.querySelector(".second").innerHTML = day;
+    document.querySelector(".day").innerHTML = textDay;
+    document.querySelector(".hour").innerHTML = textHour;
+    document.querySelector(".minute").innerHTML = textMinute;
+    document.querySelector(".second").innerHTML = textSecond;
 
 
 }
